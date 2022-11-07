ed8a657e6bd5d36ce1d36684d1d8469575a5a51b
baba-s <shotababa1206@gmail.com>
2022-11-07 19:27:36 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ContentSizeFitterDisableDirty
+# Kogane Content Size Fitter Disable Dirty
+
+シーンを開いた時に ContentSizeFitter がシーンの Dirty フラグを立ててしまう問題を解消するコンポーネント
---
+++ Runtime/ContentSizeFitterDisableDirty.cs
@@ -0,0 +1,67 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+    [DisallowMultipleComponent]
+    [ExecuteAlways]
+    [RequireComponent( typeof( TMP_Text ) )]
+    [RequireComponent( typeof( ContentSizeFitter ) )]
+    internal sealed class ContentSizeFitterDisableDirty : MonoBehaviour
+    {
+        private TMP_Text          m_tmpTextCache;
+        private ContentSizeFitter m_contentSizeFitterCache;
+
+        private TMP_Text          TmpText           => m_tmpTextCache != null ? m_tmpTextCache : m_tmpTextCache = GetComponent<TMP_Text>();
+        private ContentSizeFitter ContentSizeFitter => m_contentSizeFitterCache != null ? m_contentSizeFitterCache : m_contentSizeFitterCache = GetComponent<ContentSizeFitter>();
+
+        private string m_text;
+
+        private static bool IsPlaying =>
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying;
+#else
+            true;
+#endif
+
+        private void Reset()
+        {
+            ContentSizeFitter.enabled = false;
+        }
+
+        private void Awake()
+        {
+            if ( !IsPlaying ) return;
+
+            ContentSizeFitter.enabled = true;
+        }
+
+        private void Update()
+        {
+            if ( IsPlaying ) return;
+
+            var currentText = TmpText.text;
+
+            if ( m_text == null )
+            {
+                m_text = currentText;
+                return;
+            }
+
+            if ( m_text != currentText )
+            {
+                Apply();
+            }
+
+            m_text = currentText;
+        }
+
+        private void Apply()
+        {
+            ContentSizeFitter.enabled = true;
+            Canvas.ForceUpdateCanvases();
+            ContentSizeFitter.enabled = false;
+        }
+    }
+}
